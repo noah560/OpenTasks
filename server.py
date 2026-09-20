20be5a3ce9229a35eddf1494fd6d317f5448e35f
Noah Scarborough <thisisnotmyemail@ilikemyprivacy.com>
2026-09-20 10:03:10 -0500

    Changed UserHandle class to match the new protocol.

--- server.py
+++ server.py
@@ -14,161 +14,66 @@ class UserHandle:
         # So we can just use concatenation
         # on all OSs later
         self.path = os.path.join(path, "")
-    def get_key(self):
-        with open(self.path + "keyfile", "rb") as f:
+    def read_file(self, name):
+        with open(self.path + name, "rb") as f:
             d = f.read()
         return d
-    def get_todo(self):
-        with open(self.path + "todo", "rb") as f:
-            d = f.read()
+    def read_file_section(self, name, start, size):
+        with open(self.path + name, "rb") as f:
+            f.seek(start, 0)
+            d = f.read(size)
         return d
-    def get_notifications(self):
-        unread = os.path.join(os.path.join(
-            self.path + "notifications", "unread"
-        ), "")
-        read = os.path.join(os.path.join(
-            self.path + "notifications", "read"
-        ), "")
-        # Move unread to read
-        for n in os.listdir(unread):
-            os.rename(unread + n, read + n)
-        read_list = os.listdir(read)
-        data = b""
-        for n in read_list:
-            with open(read + n, "rb") as f:
-                data += n.encode("utf-8")
-                data += b"\n"
-                data += f.read().strip()
-                data += b"\n"
-        for n in read_list:
-            passed = time.time() - int(n)
-            if passed > (DAY * 1):
-                os.remove(read + n)
+    def get_file_size(self, name):
+        with open(self.path + name, "rb") as f:
+            f.seek(0, 2)
+            size = f.tell()
+        return size
+    def write_file(self, name, data):
+        with open(self.path + name, "wb") as f:
+            f.write(data)
+    def write_file_section(self, name, start, data):
+        with open(self.path + name, "r+b") as f:
+            f.seek(start, 0)
+            f.write(data)
+    def append(self, name, data):
+        with open(self.path + name, "ab") as f:
+            f.write(data)
+    def delete(self, name):
+        os.remove(self.path + name)
+    def list_files(self):
+        return os.listdir(self.path)
+    def push_line(self, name, data):
+        with open(self.path + name, "r+b") as f:
+            f.seek(-1, 2)
+            if f.read(1) != b"\n":
+                f.write(b"\n")
+            f.write(data)
+    def pop_ending_line(self, name):
+        d = []
+        with open(self.path + name, "rb") as f:
+            for line in f:
+                d.append(f.read().strip("\r\n"))
+        if len(d[-1]) == 0:
+            d.pop()
+        data = d.pop()
+        with open(self.path + name, "wb") as f:
+            for i in d:
+                f.write(i)
+                f.write(b"\n")
         return data
-    def get_upcoming_events(self, days):
-        data = b""
-        now = time.time()
-        with open(self.path + "events", "rb") as f:
-            a = None
+    def pop_starting_line(self, name):
+        d = []
+        with open(self.path + name, "rb") as f:
             for line in f:
-                if a is None:
-                    a = int(line.strip())
-                else:
-                    b = line.strip()
-                    # Process the pair of lines
-                    remaining = a - now
-                    if remaining > 0 and remaining < DAY * days:
-                        data += str(a).encode("utf-8")
-                        data += b"\n"
-                        data += b
-                        data += b"\n"
-                    # Reset for next round
-                    a = None
+                d.append(f.read().strip("\r\n"))
+        if len(d[-1]) == 0:
+            d.pop()
+        data = d.pop(0)
+        with open(self.path + name, "wb") as f:
+            for i in d:
+                f.write(i)
+                f.write(b"\n")
         return data
-    def get_all_events(self):
-        with open(self.path + "events", "rb") as f:
-            d = f.read()
-        return d
-    def set_key(self, k):
-        with open(self.path + "keyfile", "wb") as f:
-            f.write(k)
-    def add_todo(self, index, name):
-        d = []
-        with open(self.path + "todo", "rb") as f:
-            for line in f:
-                if line.endswith(b"\n"):
-                    l = line[:-1]
-                else:
-                    l = line
-                if len(l) != 0:
-                    d.append(line)
-        index = min(index, int(len(d) // 2))
-        # Pushes ones in front back so that
-        # name actually comes first
-        d.insert(index * 2, b"false")
-        d.insert(index * 2, name.encode("utf-8"))
-        with open(self.path + "todo", "wb") as f:
-            for line in d:
-                f.write(line)
-                f.write(b"\n")
-    def remove_todo(self, index):
-        d = []
-        with open(self.path + "todo", "rb") as f:
-            for line in f:
-                if line.endswith(b"\n"):
-                    l = line[:-1]
-                else:
-                    l = line
-                if len(l) != 0:
-                    d.append(line)
-        index = min(index, int(len(d) // 2) - 1)
-        del d[index * 2]
-        del d[index * 2]
-        with open(self.path + "todo", "wb") as f:
-            for line in d:
-                f.write(line)
-                f.write(b"\n")
-    def modify_todo(self, index, done):
-        d = []
-        with open(self.path + "todo", "rb") as f:
-            for line in f:
-                if line.endswith(b"\n"):
-                    l = line[:-1]
-                else:
-                    l = line
-                if len(l) != 0:
-                    d.append(line)
-        index = min(index, int(len(d) // 2) - 1)
-        d[index * 2 + 1] = b"true" if done else b"false"
-        with open(self.path + "todo", "wb") as f:
-            for line in d:
-                f.write(line)
-                f.write(b"\n")
-    def push_notification(self, name):
-        unread = os.path.join(os.path.join(
-            self.path + "notifications", "unread"
-        ), "")
-        timestamp = int(time.time())
-        lst = os.listdir(unread)
-        while str(timestamp) in lst:
-            timestamp += 1
-        timestamp = str(timestamp)
-        with open(unread + timestamp, "wb") as f:
-            f.write(name)
-    def add_event(self, timestamp, name):
-        with open(self.path + "events", "rb") as f:
-            d = f.read()
-        if not d.endswith(b"\n"):
-            d += b"\n"
-        d += str(timestamp).encode("utf-8")
-        d += b"\n"
-        d += name
-        d += b"\n"
-        with open(self.path + "events", "wb") as f:
-            f.write(d)
-    def remove_event(self, timestamp, name):
-        d = []
-        with open(self.path + "events", "rb") as f:
-            for line in f:
-                if line.endswith(b"\n"):
-                    l = line[:-1]
-                else:
-                    l = line
-                if len(l) != 0:
-                    d.append(line)
-        del_id = None
-        for i in range(int(len(d)//2)):
-            if d[2 * i] == str(timestamp).encode("utf-8"):
-                if d[2 * i + 1] == name.encode("utf-8"):
-                    del_id = i
-                    break
-        if del_id is not None:
-            del d[2 * del_id + 1]
-            del d[2 * del_id]
-        with open(self.path + "events", "wb") as f:
-            for line in d:
-                f.write(line)
-                f.write(b"\n")
 
 
 class DataManager:
@@ -301,45 +206,7 @@ try:
                 response = None
                 user = data.get_user(username)
                 try:
-                    if request_type == 0:
-                        days = int(request_body[0])
-                        response = user.get_upcoming_events(days)
-                    elif request_type == 1:
-                        response = user.get_todo()
-                    elif request_type == 2:
-                        response = b""
-                        index = int(request_body[0])
-                        done = request_body[1] == "true"
-                        user.modify_todo(index, done)
-                    elif request_type == 3:
-                        response = b""
-                        index = int(request_body[0])
-                        name = request_body[1]
-                        user.add_todo(index, name)
-                    elif request_type == 4:
-                        response = b""
-                        index = int(request_body[0])
-                        user.remove_todo(index)
-                    elif request_type == 5:
-                        response = b""
-                        timestamp = int(request_body[0])
-                        name = request_body[1]
-                        user.add_event(timestamp, name)
-                    elif request_type == 6:
-                        response = b""
-                        timestamp = int(request_body[0])
-                        name = request_body[1]
-                        user.remove_event(timestamp, name)
-                    elif request_type == 7:
-                        response = b""
-                        name = request_body[0]
-                        user.push_notification(name)
-                    elif request_type == 8:
-                        response = user.get_notifications()
-                    elif request_type == 9:
-                        response = user.get_all_events()
-                    else:
-                        break
+                    # TODO!TODO
                     conn.sendall(len(response).to_bytes(
                         2, byteorder="big"
                     ))
